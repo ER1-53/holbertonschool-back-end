2ded2badaf26940a97fb4782f7b9c539159aab8b
ER1-53 <cremey.erwan@gmail.com>
2023-12-22 14:12:23 +0100

    update task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,19 +11,21 @@ def appeler_api():
     task = requests.get(
         "https://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1]))
 
-    if user.status_code == 200 and task.status_code == 200:
-        user_data = user.json()
-        tasks_data = task.json()
+    user_data = user.json()
+    tasks_data = task.json()
 
-        EMPLOYEE_NAME = user_data["name"]
-        TOTAL_NUMBER_OF_TASKS = len(tasks_data)
-        NUMBER_OF_DONE_TASKS = 0
-        print("Employee {} is done with tasks ({}/{}):".format(
-            EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-        for task in tasks_data:
-            if task["completed"]:
-                NUMBER_OF_DONE_TASKS += 1
-                print("\t {}".format(task["title"]))
+    EMPLOYEE_NAME = user_data["name"]
+    TOTAL_NUMBER_OF_TASKS = len(tasks_data)
+    NUMBER_OF_DONE_TASKS = 0
+    for task in tasks_data:
+        if task["completed"]:
+            NUMBER_OF_DONE_TASKS += 1
+
+    print("Employee {} is done with tasks ({}/{}):".format(
+    EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+    for task in tasks_data:
+        if task["completed"]:
+            print("\t {}".format(task["title"]))
 
 
 if __name__ == '__main__':
