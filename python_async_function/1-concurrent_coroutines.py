e90639559e8cc4f43139bc9f24b7a7544ec55ee6
ER1-53 <cremey.erwan@gmail.com>
2024-01-02 21:03:21 +0100

    mypy with typing correction

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 """ running win """
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """delay_n = await asyncio.gather(
         *[wait_random(max_delay) for _ in range(n)])"""
     list_of_delay = []
--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import asyncio
+from typing import List
 """ return task """
 task_wait_random = __import__('3-tasks').wait_random
 
 
-async def task_wait_n(n: int, max_delay: int) -> float:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """task delay"""
     list_of_delay = []
     for turn in range(n):
